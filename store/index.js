db9c7000173cb475c67d11c9b3b70bea6dafc6a0
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-08-05 11:04:16 +0300

    site url changed

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-const siteURL = "https://therapy.korkinetim.com"
+const siteURL = "https://sites.korkinetim.com"
 
 export const state = () => ({
   posts: [],
@@ -20,7 +20,7 @@ export const actions = {
 
     try {
       let posts = await fetch(
-        `${siteURL}/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`
+        `${siteURL}/wp-json/wp/v2/projects?page=1&per_page=20&_embed=1`
       ).then(res => res.json())
 
       posts = posts
