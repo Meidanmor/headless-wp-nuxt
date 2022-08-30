39a7009d1287e2f21d2e49a68614c577733738f6
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-08-30 16:02:11 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -213,11 +213,11 @@ a.readmore {
 }
 
 @keyframes rotation{
-	0%{
-		transform: rotate(0);
-	}
-	100%{
-		transform: rotate(360deg);
-	}
+0%{
+transform: rotate(0);
+}
+100%{
+transform: rotate(360deg);
+}
 }
 </style>
